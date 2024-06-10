1f82ecb7c6417ea0b0c679d8e6a21b6b1e906b6d
Matteo Giliotti <matteogiliotti@gmail.com>
2024-06-10 13:44:49 -0400

    sizing fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,11 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import 'animate.css';
 
-// 
-
-
-const soccerPlayers: {[key: string]: string} = {
+const soccerPlayers: { [key: string]: string } = {
   nesta: "Italy",
   klose: "Germany",
   henry: "France",
-  pires: "France", 
+  pires: "France",
   pirlo: "Italy",
   vieri: "Italy",
   keane: "Ireland",
@@ -20,7 +17,7 @@ const soccerPlayers: {[key: string]: string} = {
   pique: "Spain",
   ramos: "Spain",
   viera: "France",
-  silva: "Brazil",
+  silva: "Spain/ Portugal",
   petit: "France",
   kafes: "Greece",
   lakis: "Greece",
@@ -54,6 +51,8 @@ function App() {
   const [guessCount, setGuessCount] = useState<number>(6);
   const [players, setPlayers] = useState<Set<string>>(new Set(Object.keys(soccerPlayers)))
   const [hint, setHint] = useState<string>("");
+  const [ruleDisplay, setRuleDisplay] = useState<boolean>(false);
+  const [ruleDisplayClass, setRuleDisplayClass] = useState<string>("animate__animated animate__fadeIn")
 
   interface CountInfo {
     count: number;
@@ -70,12 +69,12 @@ function App() {
   }, []);
 
   const newWord = async () => {
-        if(players.size < 1) setPlayers(new Set(Object.keys(soccerPlayers)))
-        
-        let idx = Math.floor(Math.random() * players.size)
-        const newWord: string = Object.keys(soccerPlayers)[idx];
-        setHint(soccerPlayers[newWord])
-        setStrWord(newWord);   
+    if (players.size < 1) setPlayers(new Set(Object.keys(soccerPlayers)))
+
+    let idx = Math.floor(Math.random() * players.size)
+    const newWord: string = Object.keys(soccerPlayers)[idx];
+    setHint(soccerPlayers[newWord])
+    setStrWord(newWord);
   }
 
   const handleReset = () => {
@@ -123,7 +122,7 @@ function App() {
   const checkGuess = (g: string) => {
     console.log(strWord);
 
-    const checkCount = {} as {[key : string]: CountInfo}
+    const checkCount = {} as { [key: string]: CountInfo }
 
     for (let i = 0; i < 5; i++) {
       if (checkCount[strWord[i]]) {
@@ -182,11 +181,29 @@ function App() {
     return pattern.test(string);
   };
 
+  const handleRuleDisplay = () => {
+    if (ruleDisplay) {
+      setRuleDisplayClass("animate__animated animate__fadeOutDown")
+      
+      setTimeout(() => {
+        setRuleDisplay(!ruleDisplay)
+      }, 1000)
+
+    } else{
+      setRuleDisplayClass("animate__animated animate__fadeInUp")
+      setRuleDisplay(!ruleDisplay)
+
+    }
+  }
+
 
   return (
     <>
-      <p>{gameWon ? "Congratulations" : "Guess the Euro Player"}</p>
-      <p className='animate__animated animate__fadeIn'>{guessCount < 3 ? `Hint: ${hint}` : ""}</p>
+      <h1>Euro Wordle</h1>
+      <h4>Guess the European Championship Player from 2000 - 2020</h4>
+      <button onClick={handleRuleDisplay}>Rules</button>
+      <p>{gameWon ? "Congratulations" : ""}</p>
+      <p>{guessCount < 3 ? `Hint: ${hint}` : ""}</p>
       <p>{guessCount > 0 ? `Guess Left: ${guessCount}` : `Game Over! The player was ${strWord}`}</p>
       <input
         style={{ margin: "0 0 8px 0", minWidth: "300px", minHeight: "40px", fontSize: "30px", textAlign: "center" }}
@@ -202,11 +219,11 @@ function App() {
 
       <div>
         {guesses.map((guessObj: any, i: number) => (
-          <div key={i} style={{ display: "flex", justifyContent: "center", alignItems: "center"}}>
+          <div key={i} style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
             {guessObj.result.map((letter: string, index: number) => {
+              
               console.log(guessObj)
               const l = guessObj.guess[index];
-
               return (
                 <div
                   key={index}
@@ -221,7 +238,7 @@ function App() {
                     margin: "2px",
                   }
                   }
-                  className={`animate__animated animate__flipInX animate-delay-${i}`}
+                  className={`animate__animated animate__flipInX animate__delay-${index}s`}
                 >
                   <p style={{ color: letter === "r" ? "white" : "black", fontSize: "30px", padding: 0, margin: 0 }}>
                     {l.toLowerCase()}
@@ -232,7 +249,20 @@ function App() {
           </div>
         ))}
       </div>
-      <button style={{ display: gameWon || guessCount === 0 ? "block" : "none", margin: "auto", marginTop: "8px"}} className="animate__animated animate__fadeIn" onClick={handleReset} type="button">Reset</button>
+
+      <button style={{ display: gameWon || guessCount === 0 ? "block" : "none", margin: "auto"}} className="animate__animated animate__fadeInUp" onClick={handleReset} type="button">Reset</button>
+
+      <div onClick={handleRuleDisplay} className={ruleDisplayClass} style={{cursor: "pointer", background: "black", position: "absolute", display: ruleDisplay ? "flex" : "none", alignContent: "center", justifyContent: "center", verticalAlign: "middle", flexDirection: "column", border: "1px solid white", top: "20%", left:"0", right: "0", marginLeft: "auto", marginRight: "auto", padding: "2%", maxWidth: "500px", width:"90%", minWidth: "420px" }}>
+        <h3 style={{ padding: 0, margin: 0 }}>Rules</h3>
+        <div style={{ justifyContent: "left", alignItems: "center", display: "flex", maxWidth: "500px", marginLeft: "20%" }}>
+          <div style={{ height: "20px", width: "20px", background: "yellow", display: "inline-block", color: "black" }}>e</div><span style={{ verticalAlign: "middle", marginLeft: "2px" }}> means the letter is somewhere in the word</span>
+        </div>
+        <div style={{ justifyContent: "left", alignItems: "center", display: "flex", maxWidth: "500px", marginLeft: "20%" }}>
+          <div style={{ height: "20px", width: "20px", background: "green", display: "inline-block", color: "black" }}>e</div><span style={{ verticalAlign: "middle", marginLeft: "2px" }}> means the letter is in the correct position</span>
+        </div>
+
+      </div>
+
     </>
   );
 }
